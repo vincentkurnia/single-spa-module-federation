593ce310e228c69bd12c67228e416d3250004617
vincentkurnia <39484010+vincentkurnia@users.noreply.github.com>
2022-05-18 11:40:02 +0700

    add webpack config

---
+++ another/webpack.config.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const port = 3003;
+const packageJson = require("./package.json");
+
+module.exports = {
+    mode: "development",
+
+    output: {
+        publicPath: `http://localhost:${port}/`
+    },
+
+    devServer: {
+        port: port,
+        historyApiFallback: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                        plugins: ["@babel/plugin-transform-runtime"]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new ModuleFederationPlugin({
+            name: "container",
+            // library: { type: "var", name: "container" },
+            filename: "remoteEntry.js",
+            remotes: {
+
+            },
+            exposes: {},
+            shared: packageJson.dependencies
+        })
+    ]
+}
