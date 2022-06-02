f4f60d0d8ce082e61e1d84b471af4bc44ccc51d0
vincentkurnia <39484010+vincentkurnia@users.noreply.github.com>
2022-06-02 09:49:23 +0700

    new registerApplication

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,28 +1,53 @@
 import { registerApplication, start } from "single-spa";
 
-registerApplication(
-    "navbar",
-    () => import("navigation/Navbar"),
-    (location) => location.pathname.startsWith("/")
-);
+// registerApplication(
+//     "navbar",
+//     () => import("navigation/Navbar"),
+//     (location) => location.pathname.startsWith("/")
+// );
 
-registerApplication(
-    "home",
-    () => import("navigation/Home"),
-    (location) => location.pathname.startsWith("/")
-);
+// registerApplication(
+//     "home",
+//     () => import("navigation/Home"),
+//     (location) => location.pathname.startsWith("/")
+// );
 
-registerApplication(
-    "demo",
-    () => import("demo/Demo"),
-    (location) => location.pathname.startsWith("/demo")
-);
+// registerApplication(
+//     "demo",
+//     () => import("demo/Demo"),
+//     (location) => location.pathname.startsWith("/demo")
+// );
 
-registerApplication(
-    "another",
-    () => import("another/Another"),
-    (location) => location.pathname.startsWith("/another")
-);
+// registerApplication(
+//     "another",
+//     () => import("another/Another"),
+//     (location) => location.pathname.startsWith("/another")
+// );
+
+registerApplication({
+    name: "navbar",
+    app: () => import("navigation/Navbar"),
+    activeWhen: ["/"]
+});
+
+registerApplication({
+    name: "home",
+    app: () => import("navigation/Home"),
+    activeWhen: ["/"]
+});
+
+registerApplication({
+    name: "demo",
+    app: () => import("demo/Demo"),
+    activeWhen: ["/demo"]
+});
+
+registerApplication({
+    name: "another",
+    app: () => import("another/Another"),
+    activeWhen: ["/another"]
+});
+
 
 start({
     urlRerouteOnly: true
